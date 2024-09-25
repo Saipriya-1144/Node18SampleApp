41be67c15b706a5f0cdb41340e0728e6c4f9c494
Saipriya-1144 <saipriyach1144@gmail.com>
2024-09-25 08:10:39 +0000

    for 'githubactions tests' from pipeline

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/dummy', (req, res) => {
 
 app.get('/placeholder', (req, res) => {
     console.log('Placeholder endpoint called!');
-    res.send('<<<node-place-holder>>>');
+    res.send('782f0b69-8280-498e-990c-496689fa0f1c');
 })
 
 app.listen(port, () => console.log('Listening on port %s', port));
