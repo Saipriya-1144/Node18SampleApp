7a934558bb9a58e50ebf663f3b09f654e3082315
Saipriya-1144 <saipriyach1144@gmail.com>
2024-09-29 20:43:17 +0530

    modified server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/dummy', (req, res) => {
 
 app.get('/placeholder', (req, res) => {
     console.log('Placeholder endpoint called!');
-    res.send('6b6a07df-26a3-42c3-89ff-0cc612022859');
+    res.send('<<<node-place-holder>>>');
 })
 
 app.listen(port, () => console.log('Listening on port %s', port));
