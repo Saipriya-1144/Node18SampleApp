4e6f53a63bb49ad913297bb55b9944b71ab59bf3
Saipriya-1144 <saipriyach1144@gmail.com>
2024-09-30 08:25:11 +0530

    modified server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log('Hello endpoint called!');
-    res.send('Hello from NodeJs App, Heyyy!')
+    res.send('Hello from NodeJs App, Heyyy holaaa!')
 })
 
 app.get('/dummy', (req, res) => {
